a34c41fc79ab944223bef6bb5f9dfe804a7e3949
Bartosz Janiak <janiak.bartosz92@gmail.com>
2020-12-02 22:01:47 +0100

    Fix route settings

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const cors = require('cors');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+app.use(cors());
+app.use(express.static(path.join(__dirname + '/client/public')));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use('/api', testimonialsRoutes);
